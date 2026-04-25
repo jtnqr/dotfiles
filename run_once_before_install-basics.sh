586a49a91bd8e8cbd503ba27d01613148d5f4497
Julius Wahyu Wiccaksono <juliuspanjul25@gmail.com>
2026-04-26 05:35:20 +0700

    add bootstrap script, .gitignore, and fix zshrc template

---
+++ run_once_before_install-basics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# install vim-plug if missing
+if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
+    curl -fLo "$HOME/.vim/autoload/plug.vim" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# install fzf if missing (the version that supports --zsh)
+if [ ! -d "$HOME/.fzf" ]; then
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --bin
+fi
