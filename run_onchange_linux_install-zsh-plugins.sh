ff28e9270514c74719b2ea38499c39d79fbd6376
Julius Wahyu Wiccaksono <juliuspanjul25@gmail.com>
2026-05-15 15:57:12 +0700

    add linux prefix and output tracking to zsh plugin script

--- run_onchange_linux_install-zsh-plugins.sh
+++ run_onchange_linux_install-zsh-plugins.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+# --- Linux Zsh Plugins Installation ---
+echo "==> Verifying Zsh plugins via Oh My Zsh..."
+
 ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
 
-# Function to clone plugin if missing
 install_plugin() {
     if [ ! -d "$ZSH_CUSTOM/plugins/$1" ]; then
-        echo "Installing $1..."
+        echo "    [!] Installing $1..."
         git clone https://github.com/$2 "$ZSH_CUSTOM/plugins/$1"
+    else
+        echo "    [✓] $1 is already installed."
     fi
 }
 
@@ -13,3 +17,5 @@ install_plugin "zsh-syntax-highlighting" "zsh-users/zsh-syntax-highlighting.git"
 install_plugin "zsh-autosuggestions" "zsh-users/zsh-autosuggestions.git"
 install_plugin "you-should-use" "MichaelAquilina/zsh-you-should-use.git"
 install_plugin "evalcache" "mroth/evalcache.git"
+
+echo "==> Zsh plugins verification complete."
